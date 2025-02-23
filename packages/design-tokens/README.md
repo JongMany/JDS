e1fa338696f1bf13bc670246b36153d91578b94d
JongMany <blackberry1114@naver.com>
2025-02-23 11:50:22 +0900

    chore: rollup 세팅

---
+++ packages/design-tokens/README.md
@@ -0,0 +1,8 @@
+# Design-Tokens
+
+
+
+### Package.json 분석
+main: cjs 진입점
+module: esm 진입점
+types: 타입정의 파일 제공
---
+++ packages/design-tokens/rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import filesize from "rollup-plugin-filesize";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs", // CommonJS 출력
+      exports: "auto",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm", // ES Module 출력
+    },
+  ],
+  plugins: [
+    resolve(), //node_modules 지원
+    commonjs(), // rollup은 cjs 패키지를 해석할 수 없음
+    typescript({ tsconfig: "./tsconfig.json" }), // 타입스크립트 변환
+    babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    }),
+    terser(), // 번들 파일 압축 및 코드 난독화
+    filesize(), // 번들 크기 출력을 위함
+  ],
+};
