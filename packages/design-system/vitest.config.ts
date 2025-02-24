cc0a8f2724a918003b42a4d361c5ed642ec8fefb
JongMany <blackberry1114@naver.com>
2025-02-24 21:19:21 +0900

    chore: vite / vitest 세팅

---
+++ packages/design-system/vitest.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+
+import { defineConfig } from 'vitest/config';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./setupTests.ts'],
+    coverage: {
+      provider: 'v8',
+      exclude: [
+        '**/index.ts',
+        '**/*.model.ts',
+        '**/*.d.ts',
+        '**/*.stories.tsx',
+        '**/*.css.ts',
+        './.storybook',
+        './dist',
+        '.eslintrc.cjs',
+        'rollup.config.js',
+        'vitest.config.ts',
+        'vite.config.ts',
+        './src/storybook-units',
+      ],
+    },
+  },
+  plugins: [vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
