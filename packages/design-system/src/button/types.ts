51b027c16e6bcc3e918bbb42297f61bef570206e
JongMany <blackberry1114@naver.com>
2025-02-26 07:48:04 +0900

    feat: useButton hooks

--- packages/design-system/src/button/types.ts
+++ packages/design-system/src/button/types.ts
@@ -1,11 +1,46 @@
-import { ColorProps, Size } from '../types';
+import { ComponentProps, HTMLAttributes, KeyboardEvent } from "react";
+import { ColorProps, Size } from "../types";
 
 export type ButtonProps = {
-  color?: ColorProps['color'];
+  color?: ColorProps["color"];
   isDisabled?: boolean;
   isLoading?: boolean;
   size?: Size;
-  variant?: 'solid' | 'outline' | 'ghost';
+  variant?: "solid" | "outline" | "ghost";
   leftIcon?: React.ReactNode;
   rightIcon?: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type ButtonElementType = "button" | "a" | "div" | "span" | "input";
+export type ButtonEventType = KeyboardEvent<HTMLButtonElement> &
+  KeyboardEvent<HTMLAnchorElement> &
+  KeyboardEvent<HTMLDivElement> &
+  KeyboardEvent<HTMLSpanElement> &
+  KeyboardEvent<HTMLInputElement>;
+
+export type BaseButtonProps<T extends ButtonElementType = "button"> = {
+  elementType?: T;
+  role?: string;
+  type?: "button" | "submit" | "reset";
+  isDisabled?: boolean;
+  isLoading?: boolean;
+  tabIndex?: number;
+} & ComponentProps<T>;
+
+export type UseButtonReturn<T> = {
+  buttonProps: HTMLAttributes<T> & {
+    role?: string;
+    type?: "button" | "submit" | "reset";
+    tabIndex?: number;
+    disabled?: boolean;
+    "data-loading": boolean;
+  };
+};
+
+export type OverloadedButtonFunction = {
+  (props: BaseButtonProps<"button">): UseButtonReturn<HTMLButtonElement>;
+  (props: BaseButtonProps<"a">): UseButtonReturn<HTMLAnchorElement>;
+  (props: BaseButtonProps<"div">): UseButtonReturn<HTMLDivElement>;
+  (props: BaseButtonProps<"span">): UseButtonReturn<HTMLSpanElement>;
+  (props: BaseButtonProps<"input">): UseButtonReturn<HTMLInputElement>;
+};
---
+++ packages/design-system/src/button/useButton.ts
@@ -0,0 +1,99 @@
+import { BaseButtonProps, ButtonEventType } from "./types";
+
+export const useButton = (
+  props: BaseButtonProps<"a" | "button" | "div" | "input" | "span">
+) => {
+  const {
+    elementType = "button",
+    role = "button",
+    type = "button",
+    isDisabled = false,
+    isLoading = false,
+    tabIndex = 0,
+    onKeyDown,
+    ...rest
+  } = props;
+
+  const disabled = isDisabled || isLoading;
+
+  const handleKeyDown = (e: ButtonEventType) => {
+    onKeyDown?.(e);
+
+    if (e.key === " " || e.key === "Spacebar" || e.key === "32") {
+      if (disabled) return;
+      if (e.defaultPrevented) return;
+      if (elementType === "button") return;
+
+      e.preventDefault();
+      e.currentTarget.click();
+      return;
+    }
+
+    if (e.key === "Enter" || e.key === "13") {
+      if (disabled) return;
+      if (e.defaultPrevented) return;
+      if (elementType === "input" && type !== "button") return;
+
+      e.preventDefault();
+      e.currentTarget.click();
+      return;
+    }
+  };
+
+  const baseProps = {
+    ...rest,
+    "data-loading": isLoading,
+    tabIndex: disabled ? undefined : (tabIndex ?? 0),
+    onKeyDown: handleKeyDown,
+  };
+
+  let additionalProps = {};
+
+  switch (elementType) {
+    case "button": {
+      additionalProps = {
+        type: type ?? "button",
+        disabled,
+      };
+      break;
+    }
+    case "a": {
+      const { href, target, rel } = props as BaseButtonProps<"a">;
+
+      additionalProps = {
+        role: "button",
+        href: disabled ? undefined : href,
+        target: disabled ? undefined : target,
+        rel: disabled ? undefined : rel,
+        "area-disabled": isDisabled,
+      };
+      break;
+    }
+    case "input": {
+      additionalProps = {
+        role: "button",
+        type: props.type,
+        disabled,
+        "area-disabled": undefined,
+      };
+      break;
+    }
+    default: {
+      additionalProps = {
+        role: "button",
+        type: type ?? "button",
+        "area-disabled": isDisabled,
+      };
+      break;
+    }
+  }
+
+  const buttonProps = {
+    ...baseProps,
+    ...additionalProps,
+  };
+
+  return {
+    buttonProps,
+  };
+};
