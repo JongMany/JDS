49ae11de3599d541d8b3bca70a060f715fab0513
JongMany <blackberry1114@naver.com>
2025-03-02 16:01:26 +0900

    chore: eslint 설정 수정

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,9 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
+import onlyWarn from 'eslint-plugin-only-warn';
+import importPlugin from 'eslint-plugin-import';
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +17,36 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      import: importPlugin,
+      // '@typescript-eslint': ['@typescript-eslint/eslint-plugin'],
     },
     rules: {
-      "turbo/no-undeclared-env-vars": "warn",
+      'turbo/no-undeclared-env-vars': 'warn',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            // 타입 import가 먼저 오도록 설정
+            ['type'],
+            ['builtin', 'external'],
+            ['internal'],
+            ['parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'always', // 그룹 간에 새로운 줄 추가
+          alphabetize: {
+            order: 'asc', // 알파벳 순으로 정렬
+            caseInsensitive: true,
+          },
+        },
+      ],
+      // import type {} from ""으로 강제
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports', // 'import type'을 선호
+          disallowTypeAnnotations: false, // 타입 어노테이션을 별도로 사용하는 것을 허용
+        },
+      ],
     },
   },
   {
@@ -27,6 +55,6 @@ export const config = [
     },
   },
   {
-    ignores: ["dist/**"],
+    ignores: ['dist/**'],
   },
 ];
