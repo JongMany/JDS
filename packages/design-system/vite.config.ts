50ef2a2ce5ce9184fcfaabca5f01d6289215612d
JongMany <blackberry1114@naver.com>
2025-03-02 15:33:59 +0900

    chore: 절대 경로 설정 추가

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,12 +1,16 @@
-import path from "path";
+import path from 'path';
 
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default {
   plugins: [vanillaExtractPlugin()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@components/*': path.resolve(__dirname, 'src/components'),
+      '@styles/*': path.resolve(__dirname, 'src/styles'),
+      '@utils/*': path.resolve(__dirname, 'src/utils'),
     },
   },
 };
